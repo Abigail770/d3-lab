e04a974a7f18dfe121827a8720d0a2b4d8e4ec7a
Abby Gleason <54085652+Abigail770@users.noreply.github.com>
2020-03-29 11:46:58 -0500

    finalize attributes and design

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 (function(){
 
 //pseudo-global variables
-var attrArray = ["var1", "var2", "var3", "var4", "var5"]; //list of attributes
+var attrArray = ["Centers for Homeless Persons", "At-risk-of-poverty rate", "Homes with some type of computer", "Average Household Income (thousands)", "Average Expenditure per Household (thousands)"]; //list of attributes
 var expressed = attrArray[0]; //initial attribute
     
 //chart frame dimensions
@@ -39,9 +39,6 @@ function setMap(){
 
     //create Albers equal area conic projection centered on France
     var projection = d3.geoConicConformalSpain()
-//        .center([-11, 36])
-//        .rotate([-2, 0, 0])
-//        .parallels([43, 62])
         .scale(2000)
         .translate([width / 2, height / 2]);
 
@@ -70,9 +67,6 @@ function setMap(){
             .attr("class", "countries")
             .attr("d", path);
         
-        var mapBackground = map.append("path")
-            .attr("class", "mapBackground"); //assign class for styling
-        
         //create the color scale
         var colorScale = makeColorScale(csvData);
 
@@ -90,7 +84,7 @@ function setMap(){
     
 function joinData(spainProvinces, csvData){
         //variables for data join
-        var attrArray = ["var1", "var2", "var3", "var4", "var5"];
+        var attrArray = ["Centers for Homeless Persons", "At-risk-of-poverty rate", "Homes with some type of computer", "Average Household Income (thousands)", "Average Expenditure per Household (thousands)"];
 
         //loop through csv to assign each set of csv attribute values to geojson region
         for (var i=0; i<csvData.length; i++){
@@ -163,6 +157,7 @@ function makeColorScale(data){
     var domainArray = [];
     for (var i=0; i<data.length; i++){
         var csvRegion = data[i];
+        console.log(csvRegion)
         var val = parseFloat(csvRegion[expressed]);
         domainArray.push(val);
     };
@@ -241,7 +236,7 @@ function setChart(csvData, colorScale){
         .attr("x", 40)
         .attr("y", 40)
         .attr("class", "chartTitle")
-        .text("Number of Variable " + expressed[3] + " in each region");
+        .text(expressed);
 
     //create vertical axis generator
     var yAxis = d3.axisLeft(yScale)
@@ -339,7 +334,7 @@ function updateChart(bars, n, colorScale){
         });
         
     var chartTitle = d3.select(".chartTitle")
-        .text("Number of Variable " + expressed[3] + " in each region");
+        .text(expressed);
 };
     
 //function to highlight enumeration units and bars
@@ -418,21 +413,6 @@ function moveLabel(){
         .style("top", y + "px");
 };
     
-function createLegend(props){
-    // define legend
-    var legend = svg.select("body")
-        .style("fill", function(d){
-            return choropleth(d, colorScale);
-        });
-
-    // adding colored squares to legend
-    legend.append('rect') // append rectangle squares to legend                                   
-      .attr('width', legendRectSize) // width of rect size is defined above                        
-      .attr('height', legendRectSize) // height of rect size is defined above                      
-      .style('fill', color) // each fill is passed a color
-      .style('stroke', color) // each stroke is passed a color
-    }
-    
 })();
 
 //collapsible content
@@ -450,159 +430,3 @@ for (i = 0; i < accordion.length; i++) {
     }
   });
 }
-
-////execute script when window is loaded
-//window.onload = function(){
-//
-//   //SVG dimension variables
-//    var w = 900, h = 500;
-//
-//     //Example 1.2 line 1...container block
-//    var container = d3.select("body") //get the <body> element from the DOM
-//        .append("svg") //put a new svg in the body
-//        .attr("width", w) //assign the width
-//        .attr("height", h) //assign the height
-//        .attr("class", "container") //always assign a class (as the block name) for styling and future selection
-//        .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
-//    
-//    //innerRect block
-//    var innerRect = container.append("rect")
-//        .datum(400) //a single value is a DATUM
-//        .attr("width", function(d){ //rectangle width
-//            return d * 2; //400 * 2 = 800
-//        })
-//        .attr("height", function(d){ //rectangle height
-//            return d; //400
-//        })
-//        .attr("class", "innerRect") //class name
-//        .attr("x", 50) //position from left on the x (horizontal) axis
-//        .attr("y", 50) //position from top on the y (vertical) axis
-//        .style("fill", "#FFFFFF"); //fill color
-//    
-//    var dataArray = [10, 20, 30, 40, 50];
-//
-//     var cityPop = [
-//        { 
-//            city: 'Madison',
-//            population: 233209
-//        },
-//        {
-//            city: 'Milwaukee',
-//            population: 594833
-//        },
-//        {
-//            city: 'Green Bay',
-//            population: 104057
-//        },
-//        {
-//            city: 'Superior',
-//            population: 27244
-//        }
-//    ];
-//    var x = d3.scaleLinear()  //create the scale
-//        .range([90, 810]) //output min and max
-//        .domain([0, 3]); //input min and max
-//    
-//    var minPop = d3.min(cityPop, function(d){
-//        return d.population;
-//    });
-//
-//    //find the maximum value of the array
-//    var maxPop = d3.max(cityPop, function(d){
-//        return d.population;
-//    });
-//
-//    //scale for circles center y coordinate
-//    var y = d3.scaleLinear()
-//        .range([450, 50])
-//        .domain([0, 700000]);
-//    
-//     //color scale generator 
-//    var color = d3.scaleLinear()
-//        .range([
-//            "#FDBE85",
-//            "#D94701"
-//        ])
-//        .domain([
-//            minPop, 
-//            maxPop
-//        ]);
-//
-//    //Example 2.6 line 3
-//    var circles = container.selectAll(".circles") //create an empty selection
-//        .data(cityPop) //here we feed in an array
-//        .enter() //one of the great mysteries of the universe
-//        .append("circle") //inspect the HTML--holy crap, there's some circles there
-//        .attr("class", "circles")
-//        .attr("id", function(d){
-//            return d.city;
-//        })
-//        .attr("r", function(d){
-//            //calculate the radius based on population value as circle area
-//            var area = d.population * 0.01;
-//            return Math.sqrt(area/Math.PI);
-//        })
-//        .attr("cx", function(d, i){
-//            //use the scale generator with the index to place each circle horizontally
-//            return x(i);
-//        })
-//       .attr("cy", function(d){
-//            return y(d.population);
-//        })
-//        .style("fill", function(d, i){ //add a fill based on the color scale generator
-//            return color(d.population);
-//        })
-//        .style("stroke", "#000"); //black circle stroke
-//    
-//        var yAxis = d3.axisLeft(y)
-//            .scale(y);
-//    
-//         //create axis g element and add axis
-//        var axis = container.append("g")
-//            .attr("class", "axis")
-//            .attr("transform", "translate(50, 0)")
-//            .call(yAxis);   
-//    
-//        var title = container.append("text")
-//        .attr("class", "title")
-//        .attr("text-anchor", "middle")
-//        .attr("x", 450)
-//        .attr("y", 30)
-//        .text("City Populations");
-//    
-//         var labels = container.selectAll(".labels")
-//        .data(cityPop)
-//        .enter()
-//        .append("text")
-//        .attr("class", "labels")
-//        .attr("text-anchor", "left")
-//        .attr("y", function(d){
-//            //vertical position centered on each circle
-//            return y(d.population) - 1;
-//        });
-//
-//        //first line of label
-//        var nameLine = labels.append("tspan")
-//            .attr("class", "nameLine")
-//            .attr("x", function(d,i){
-//                //horizontal position to the right of each circle
-//                return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 2;
-//            })
-//            .text(function(d){
-//                return d.city;
-//            });
-//
-//        //create format generator
-//        var format = d3.format(",");
-//
-//        //second line of label
-//        var popLine = labels.append("tspan")
-//            .attr("class", "popLine")
-//            .attr("x", function(d,i){
-//                return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 2;
-//            })
-//            .attr("dy", "15") //vertical offset
-//            .text(function(d){
-//                return "Pop. " + format(d.population); //use format generator to format numbers
-//            });
-//};
